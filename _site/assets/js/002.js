aeb02e5fbd24308243fd1317abcaa47a844665f0
yoosoo2217 <yoosoo2217@g.eulji.ac.kr>
2026-08-31 17:46:58 +0900

    오류 수정

--- _site/assets/js/002.js
+++ _site/assets/js/002.js
@@ -3,7 +3,7 @@
 
 function numbers () {
 	var fields = document.getElementsByTagName("code");
-	for (field in fields) {
+	for (var field = 0; field < fields.length; field++) {
 		var num = 0;
 		var select = fields[field].innerText;
 		var select_f = select.split(/\n/);
@@ -11,7 +11,7 @@ function numbers () {
 
 		// IF YOU USE MARKDOWN AND YOU HAVE BEEN GETTING ONE ADDITIONAL LINE IN YOUR TAG CODE
 		// UNCOMMENT THE SECTION BELOW
-		
+
 		/* MARKDOWN SECTION /**/
 
 		select_f.splice(-1, 1);
@@ -20,7 +20,7 @@ function numbers () {
 
 		fields[field].innerHTML = "";
 		fields[field].appendChild(tab);
-		for (line in select_f) {
+		for (var line = 0; line < select_f.length; line++) {
 			var row = document.createElement("tr");
 			var col = document.createElement("th");
 			var colc = document.createElement("th");
